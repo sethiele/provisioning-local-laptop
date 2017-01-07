12abdf4f40b1c13e9fce68361bb8e64f033065cd
Sebastian Thiele <sebastian+git@thiele.me>
2017-01-07 12:44:23 +0100

    run ppas once

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i HOSTS setup.yml --ask-sudo-pass # --module-path ./ansible_modules
+ansible-playbook -i HOSTS setup.yml --ask-sudo-pass --skip-tags=once # --module-path ./ansible_modules
